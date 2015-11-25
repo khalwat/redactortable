90a283a4368b490e49910e0a9e34c89b3961785f
Andrew Welch <andrew@AmbrosiaSW.com>
2015-11-25 01:10:10 -0500

    Initial commit

---
+++ RedactorTablePlugin.php
@@ -0,0 +1,61 @@
+<?php
+namespace Craft;
+
+/**
+ * Redactor CSS plugin
+ */
+class RedactorTablePlugin extends BasePlugin
+{
+    function getName()
+    {
+        return Craft::t('Redactor Table');
+    }
+
+    public function getDescription()
+    {
+        return 'Adds inline tables to rich text fields in Craft CMS via the Redactor Table plugin.';
+    }
+    
+    public function getDocumentationUrl()
+    {
+        return 'https://github.com/khalwat/redactortable/blob/master/README.md';
+    }
+    
+    public function getReleaseFeedUrl()
+    {
+        return 'https://github.com/khalwat/redactortable/blob/master/releases.json';
+    }
+    
+    public function getVersion()
+    {
+        return '1.0.0';
+    }
+
+    public function getSchemaVersion()
+    {
+        return '1.0.0';
+    }
+
+    function getDeveloper()
+    {
+        return 'Megalomaniac';
+    }
+
+    function getDeveloperUrl()
+    {
+        return 'http://www.megalomaniac.com';
+    }
+
+    public function hasCpSection()
+    {
+        return false;
+    }
+
+	public function init()
+	{
+		if (craft()->request->isCpRequest())
+		{
+			craft()->templates->includeJsResource('redactortable/table.js');
+		}
+	}
+}
---
+++ readme.md
@@ -0,0 +1,34 @@
+# Redactor Table plugin for Craft CMS
+
+Adds inline tables to rich text fields in Craft CMS via the Redactor Table plugin.
+
+**Installation**
+
+1. Unzip file and place `redactortable` directory into your `craft/plugins` directory
+2.  -OR- do a `git clone https://github.com/khalwat/redactortable.git` directly into your `craft/plugins` folder.  You can then update it with `git pull`
+3. Install plugin in the Craft Control Panel under Settings > Plugins
+
+## Usage
+
+Add 'table' to the plugins array in your Redactor plugins configuration: http://buildwithcraft.com/docs/rich-text-fields#redactor-configs):
+
+```
+{
+	buttons: ['html','formatting','bold','italic','link','image'],
+	plugins: ['fullscreen', 'table'],
+    formattingTags: ['p', 'blockquote', 'pre', 'h1', 'h2', 'h3', 'h4'],
+    observeLinks: true,
+	toolbarFixedBox: true
+}
+```
+
+## Changelog
+
+### 1.0.0 -- 2015.11.25
+
+* Initial release
+
+## Credits
+
+* Imperavi for writing the plugin
+* MildlyGeeky's Redactor Font Size plugin (https://github.com/mildlygeeky/craft-redactorfontsize)
